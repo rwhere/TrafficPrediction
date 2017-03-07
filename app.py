0b4ac6ae485630a2c2d786b4a545e58b44a209d6
wlphyo <wlphyo@cpp.edu>
2017-03-06 16:40:48 -0800

    refactor + comments + add gamma

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# CS-499 | Hack-3
+# @authors: Ryan Waer & Wai Phyo
+
 from flask import Flask
 from flask import request
 import ast
@@ -7,25 +10,31 @@ import numpy as np
 
 app = Flask(__name__)
 
-input_set = [[], []]
-output_set = []
-
+# Starting with empty data sets
+# require minimum 2 dataset to train.
+# adding 2 empty data entries
 input_set = [[0, 0, 0, 0], [0, 0, 0, 0]]
 output_set = [0, 0]
-clf = svm.SVC(kernel='linear', C=1.0)
+clf = svm.SVC(kernel='linear', C=1.0, gamma=0.001)
 
 
+# Training URL
+# Example => /train?input=[1,2,3,4]&output=3
+# 1. add to data set in memory
+# 2. retrain
+# 3. store to file for next time
 @app.route('/train')
 def train():
-    print(request.args.get('input'))
-    print(request.args.get('output'))
     input_set.append(ast.literal_eval(request.args.get('input')))
     output_set.append(int(request.args.get('output')))
     clf.fit(np.array(input_set), output_set)
-    writeToFile()
+    write_to_file()
     return "trained new data"
 
-
+# Prediction
+# Example => /predict?data=[1,2,3,4]
+# 1. return predicted data
+# 2. return error in exception
 @app.route('/predict')
 def predict():
     try:
@@ -33,12 +42,13 @@ def predict():
     except Exception as e:
         return str(e)
 
-
+# Printing both input and output data set
 @app.route('/print')
-def printData():
+def print_data():
     return str(input_set) + str(output_set)
 
-def writeToFile():
+
+def write_to_file():
     print("Opening the file...")
     target = open("db.txt", 'w')
     for index in range(len(input_set)):
@@ -48,7 +58,7 @@ def writeToFile():
         target.write("%s\n" % output_set[index])
 
 
-def readFromFile():
+def read_from_file():
     print("Opening the file...")
     with open("db.txt", "r") as f:
         lines = f.readlines()
@@ -58,6 +68,8 @@ def readFromFile():
         else:
             output_set.append(lines[index].rstrip('\n'))
 
+
+# Beginning of app, read from file to add to add to data sets in memory
 if __name__ == '__main__':
-    readFromFile()
+    read_from_file()
     app.run(host='0.0.0.0', port='8804')

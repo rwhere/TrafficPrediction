21d6447f84118123ccfc37ee6573a8dadc853398
wlphyo <wlphyo@cpp.edu>
2017-03-06 19:44:51 -0800

    update python endpoint

--- FrontEnd/public/js/Hack3-ajs.js
+++ FrontEnd/public/js/Hack3-ajs.js
@@ -1,5 +1,5 @@
 var amISafeHere = angular.module('CS499Hack3', []);
-var url = 'http://127.0.0.1:8804';
+var url = 'http://ec2-35-164-83-203.us-west-2.compute.amazonaws.com:8804';
 amISafeHere.controller('Hack3Ctrl', function ($scope, $http) {
     $scope.TrainMe = function () {
         $http.get(url + "/train" + "?input=" + $('#input').val().replace(' ', '') + "&output=" + $('#output').val().replace(' ', ''))
